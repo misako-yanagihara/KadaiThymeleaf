94ac25191792bed54fa8423383a9e129f16ebdec
misako-yanagihara <yanagihara-m@mainichi.co.jp>
2023-12-25 23:37:55 +0900

    Les15課題を修正

--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -10,20 +10,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 public class OutputController {
     @PostMapping("/output")
-    public String postInput(@RequestParam(name = "val", required = false) String val, Model model) {
+    public String postInput(@RequestParam("val") String val, Model model) {
         model.addAttribute("val",val);
         return "output";
     }
 
-public class LinkController{
-
-    @GetMapping("/previous")
-    public String getLink(Model model) {
-        model.addAttribute("name", "val");
-        return "previous";
-
-    }
-}
-
 
 }
+
+
+
